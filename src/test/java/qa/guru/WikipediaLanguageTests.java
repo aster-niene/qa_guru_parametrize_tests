986299d3ebfa84718f8fa87b8859ec0f36f819cb
Ivan Ulianov <aster.niene@gmail.com>
2022-12-04 18:05:23 +0100

    added utf-8 and new test annotations: @ValueSource and @CsvSource

--- src/test/java/qa/guru/WikipediaLanguageTests.java
+++ src/test/java/qa/guru/WikipediaLanguageTests.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -29,7 +31,7 @@ public class WikipediaLanguageTests {
 
     static Stream<Arguments> wikipediaLanguageButtonsTest(){
         return Stream.of(
-                Arguments.of("en", List.of("Main page", "Contents", "Current events", "Random article", "About Wikipedia", "Contact us", "Donate")), //это количество вариантов. Если-б было несколько языков, то было-бы несколько строк
+                Arguments.of("en", List.of("Main page", "Contents", "Current events", "Random article", "About Wikipedia", "Contact us", "Donate")),
                 Arguments.of("de", List.of("Hauptseite", "Themenportale", "Zufälliger Artikel"))
         );
     }
@@ -39,5 +41,26 @@ public class WikipediaLanguageTests {
     void wikipediaLanguageButtonsTest(String locale, List<String> buttons){
         $$(".central-featured .central-featured-lang a").find(text(locale)).click();
         $$("#p-navigation a").filter(visible).shouldHave(texts(buttons));
+    }
+
+    @ParameterizedTest(name = "Checking wikipedia search for word {0}, base fo Csv file")
+    @CsvFileSource(
+            resources = "/data.csv"
+    )
+    void wikipediaLanguageButtonsTestCsvFile(String searchWord, String resultWord){
+        $$(".central-featured .central-featured-lang a").find(text("en")).click();
+        $(".vector-search-box-input").setValue(searchWord).pressEnter();
+        $(".mw-page-title-main").shouldHave(text(resultWord));
+    }
+
+    @ValueSource(strings = {
+            "Test",
+            "Program"
+            })
+    @ParameterizedTest(name = "Checking wikipedia search for word {0}")
+    void searchForWords(String word) {
+        $$(".central-featured .central-featured-lang a").find(text("en")).click();
+        $(".vector-search-box-input").setValue(word).pressEnter();
+        $(".mw-page-title-main").shouldHave(text(word));
     }
 }
